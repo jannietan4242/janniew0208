03b160aac10e45304d9920bfc7320cfaac99489a
jannietan4242 <tanjingyi94@gmail.com>
2020-10-02 10:51:55 +0800

    Update Api_event_manage.php

--- ci3/application/controllers/Api_event_manage.php
+++ ci3/application/controllers/Api_event_manage.php
@@ -2,7 +2,10 @@
 class Api_event_manage extends CI_Controller {
 
     public function backup(){
-
+        echo json_encode([
+            'status' => "OK",
+            'result' => "1",
+        ]);
     }
 
 
